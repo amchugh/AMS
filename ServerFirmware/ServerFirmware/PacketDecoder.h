e5dbc03d627f267e08fe1e51aba28bbfe2516d14
Jason McHugh <mchughj@gmail.com>
2020-01-28 15:53:50 -0800

    Minor augmentations to packet decoder
    Added some ASCII part to depict the network packets.  Added usage of
    const and data types.

--- ServerFirmware/ServerFirmware/PacketDecoder.h
+++ ServerFirmware/ServerFirmware/PacketDecoder.h
@@ -6,44 +6,69 @@
 #ifndef PACKET_DECODER_H
 #define PACKET_DECODER_H
 
-// This requires Station.h because I use some of the constants (and want them to remain
-// consistant)
-#ifndef STATION_H
+// This requires Station.h because I use some of the constants.
 #include "Station.h"
-#endif
 
+// Locations and total header size, in bytes, for various components of the
+// packet.
 #define PACKET_SENDER_LOC 0
 #define PACKET_NUMBER_LOC 1
-#define HEADER_SIZE 2
+#define HEADER_SIZE (PACKET_NUMBER_LOC+1)
+
+//
+// Packet structure
+//
+// 0           7 8         15 16        23 24        31
+// +------------+------------+------------+------------+
+// | Sender     | Number     |      Data Block 1       |
+// +------------+------------+------------+------------+
+// |         Data Block 1                 |   Data     |
+// +------------+------------+------------+------------+
+// |                   Block 2                         |
+// +------------+------------+------------+------------+
+// |                    Data Block 3                   |
+// +------------+------------+------------+------------+
+//            ...
+//
+// A data block consists of 5 bytes each:
+//
+// 0           7 8         15 16        23 24        31          39
+// +------------+------------+------------+------------+-----------+
+// | LSBs Data1 | LSBs Data2 | LSBs Data3 | LSBs Data4 |D1|D2|D3|D4|
+// +------------+------------+------------+------------+-----------+
+//                                                       2 MSBs of
+//                                                       each of the
+//                                                       four prior
+//                                                       data points
 
 struct PacketData {
-  uint8_t packetNumber;
-  uint8_t packetSenderID;
+  PacketNumber packetNumber;
+  StationIdentifier packetSenderID;
   uint16_t sampleCount;
   uint16_t samples[MAX_DATA_POINTS];
 };
 
-uint8_t getPacketNumber(byte *packet) {
-  byte num = packet[PACKET_NUMBER_LOC];  
-  return (uint8_t) num;
+PacketNumber getPacketNumber(const byte * const packet) {
+  byte result = packet[PACKET_NUMBER_LOC];
+  return (PacketNumber) result;
 }
 
-uint8_t getPacketSenderID(byte *packet) {
-  byte num = packet[PACKET_SENDER_LOC];  
-  return (uint8_t) num;
+StationIdentifier getPacketSenderID(const byte * const packet) {
+  byte result = packet[PACKET_SENDER_LOC];
+  return (StationIdentifier) result;
 }
 
 int getSampleLength(int packetLength) {
-  return (packetLength - HEADER_SIZE) / 5 * 4;  
+  return (packetLength - HEADER_SIZE) / 5 * 4;
 }
 
 /**
- * This method takes in a PacketData object to put the data into, along with a 
+ * This method takes in a PacketData object to put the data into, along with a
  * byte array to process. This allows for tracking of "most recent packet" and
  * doesn't inflate memory. Packet Length should be the length of the message as
  * the expected packet size cannot be assumed constant.
  */
-void processPacket(PacketData &p, byte *packet, int packetLength) {
+void processPacket(PacketData &p, const byte * const packet, int packetLength) {
   // Get some of the misc. information
   p.sampleCount = getSampleLength(packetLength);
   p.packetNumber = getPacketNumber(packet);
@@ -53,20 +78,19 @@ void processPacket(PacketData &p, byte *packet, int packetLength) {
   // Need to be done in groups of 4
   for (int offset = 0; offset < p.sampleCount/4; offset++) {
     p.samples[offset * 4 + 0] = packet[HEADER_SIZE + offset * 5 + 0];
-    p.samples[offset * 4 + 1] = packet[HEADER_SIZE + offset * 5 + 1];  
+    p.samples[offset * 4 + 1] = packet[HEADER_SIZE + offset * 5 + 1];
     p.samples[offset * 4 + 2] = packet[HEADER_SIZE + offset * 5 + 2];
     p.samples[offset * 4 + 3] = packet[HEADER_SIZE + offset * 5 + 3];
 
-    p.samples[offset * 4 + 0] += 
+    p.samples[offset * 4 + 0] +=
       ((uint16_t)(packet[HEADER_SIZE + (offset * 5) - 1] & 0b11000000) >> 6) << 8;
-    p.samples[offset * 4 + 1] += 
+    p.samples[offset * 4 + 1] +=
       ((uint16_t)(packet[HEADER_SIZE + (offset * 5) - 1] & 0b00110000) >> 4) << 8;
-    p.samples[offset * 4 + 2] += 
+    p.samples[offset * 4 + 2] +=
       ((uint16_t)(packet[HEADER_SIZE + (offset * 5) - 1] & 0b00001100) >> 2) << 8;
-    p.samples[offset * 4 + 3] += 
+    p.samples[offset * 4 + 3] +=
       ((uint16_t)(packet[HEADER_SIZE + (offset * 5) - 1] & 0b00000011) >> 0) << 8;
   }
-
-  return;
 }
+
 #endif
